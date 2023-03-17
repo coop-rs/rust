7b145a8132362f328895d89e5d6208b7700aaf58
Peter Kehl <peter.kehl@gmail.com>
2023-03-16 22:40:43 -0700

    CoAlloc: tests/mir-opt updated

--- tests/mir-opt/inline/inline_into_box_place.rs
+++ tests/mir-opt/inline/inline_into_box_place.rs
@@ -2,6 +2,7 @@
 // ignore-wasm32-bare compiled with panic=abort by default
 // compile-flags: -Z mir-opt-level=4
 
+// EMIT_MIR_FOR_EACH_BIT_WIDTH
 // EMIT_MIR inline_into_box_place.main.Inline.diff
 fn main() {
     let _x: Box<Vec<u32>> = Box::new(Vec::new());
